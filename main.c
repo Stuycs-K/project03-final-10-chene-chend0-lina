dad1aeb5a4a045e78a9ee1f768fa949dda19ac7a
David Chen <dchen07@pm.me>
2025-01-06 15:33:50 -0500

    Create pipes for client/server communication

--- main.c
+++ main.c
@@ -3,23 +3,31 @@
 #include <unistd.h>
 #include <errno.h>
 
-void client() {
-	printf("Clienting yes");
+void client(int in, int out) {
+	printf("Clienting yes %d %d\n", in, out);
 }
-void server() {
-	printf("Servering yes");
+void server(int in, int out) {
+	printf("Servering yes %d %d\n", in, out);
 }
+
+void fatal(const char *title) {
+	perror(title);
+	exit(errno);
+}
+
 int main(void) {
 	int child_pid;
+	int server2client[2], client2server[2];
+	if (pipe(server2client) || pipe(client2server))
+		fatal("pipe");
 	switch(child_pid = fork()) {
-		case 0:
-			client();
-			return 0;
 		case -1:
-			perror("fork");
-			return errno;
+			fatal("fork");
+		case 0:
+			client(server2client[0], client2server[1]);
+			return 0;
 		default:
-			server();
+			server(client2server[0], server2client[1]);
 			return 0;
 	}
 }
