0835fe806289d9f0822aef026436af56c0ed8eaa
echen500 <echen50@stuy.edu>
2025-01-07 01:54:42 -0500

    updated header file

--- log.h
+++ log.h
@@ -3,8 +3,9 @@
 
 void create_file(); 
 struct player_moves {char name[50]; char card[5]; char move; int winnings};
-void create_file()
-void write_file(char * data)
+void write_file(char * name, char * card, char move, int winnings );
+void read_file();
+
 #endif
 
 
