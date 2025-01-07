6c2987bd7991a80a2a74cdb0d5211f0b95b5e200
echen500 <echen50@stuy.edu>
2025-01-07 15:10:56 -0500

    added sighandler for exit

--- sigs.c
+++ sigs.c
@@ -9,11 +9,20 @@
 
 void setup_sighandler() {
     struct sigaction exit;
+    exit.sa_handler = sigint_handler;
+    exit.sa_flags = 0;
+    sigemptyset(&exit.sa_mask);
+    sigaction(SIGINT, &exit, NULL);
     struct sigaction timeout;
     timeout.sa_handler = sigalrm_handler;
     timeout.sa_flags = 0;
     sigemptyset(&timeout.sa_mask);
     sigaction(SIGALRM, &timeout, NULL);
+}
+
+void sigint_handler(int sig) {
+    printf("Exiting game.")
+    exit(0);
 }
 
 void sigalrm_handler(int sig) {
