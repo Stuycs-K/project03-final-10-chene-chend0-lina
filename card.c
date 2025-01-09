a993b0e86c594e0439d868a204900b4fe20a48c0
Abigail Lin <alin50@stuy.edu>
2025-01-09 00:38:02 -0500

    just needed to move around and rename older functions for the restrutured ones. going to work on deck.h's new ones now

--- card.c
+++ card.c
@@ -41,97 +41,86 @@ int calcHand(struct card_node *hand){
   return total;
 }
 
-// create a new deck of cards
-void createDeck(struct card_node *deck){
-  struct card_node *current = deck;
-  for (int suit = 0; suit < NUM_SUITS; suit++){
-      for (int face = 0; face < NUM_FACES; face++){
-          struct card_node *newCard = createCard(face, suit, NULL);
-          if (current == NULL){
-              deck = newCard;
-              current = deck;
-          }
-          else {
-              current->next = newCard;
-              current = newCard;
-          }
-      }
-  }
+void addCardToHand(struct card_node **hand, struct card_node *card){
+  if (!card) return;
+  card->next = *hand;
+  *hand = card;
 }
 
-void printCard(struct card_node *card){
-    if (card){
-        printf("%s of %s\n",faces[card->face],suits[card->suit]);
-    }
-}
-
-void printDeck(struct card_node *deck){
-    struct card_node *current = deck;
-    while (current){
-        printCard(current);
-        current = current->next;
-    }
-}
-
-struct card_node* insert(struct card_node *deck, int face, int suit){
-  struct card_node* newCard = createCard(face, suit, deck);
-  if (newCard == NULL){
-    return deck;
-  }
-  return newCard;
-}
-
-struct card_node * random_card(struct card_node *card){
-  if (card == NULL) return NULL;
-  int count = 0;
-  struct card_node * temp = card;
-  while (temp != NULL){
-    count++;
-    temp = temp->next;
-  }
-  int end = rand() % count;
-  for (int i = 0; i < end; i++){
-    card = card->next;
-  }
-  return card;
-}
-
-struct card_node * remove_card(struct card_node *deck, int face, int suit){
-  struct card_node *current = deck;
-  struct card_node *prev = NULL;
+void printHand(struct card_node *hand){
+  struct card_node *current = hand;
   while (current){
-    if (current->face == face && current->suit == suit){
-      if (prev){
-        prev->next = current->next;
-      }
-      else {
-        deck = current->next;
-      }
-      free(current);
-      return deck;
-    }
-    prev = current;
+    printf("%s of %s\n",faces[card->face],suits[card->suit]);
     current = current->next;
   }
-  return deck;
 }
 
-struct card_node * free_list(struct card_node *deck){
-  struct card_node *current = deck;
+void freeHand(struct card_node *hand){
+  struct card_node *current = hand;
   while (current){
     struct card_node *next = current->next;
     free_node(current);
     current = next;
   }
-  return NULL;
 }
 
-struct card_node * find_node(struct card_node *card, int face, int suit){
-  while (card != NULL){
-    if (card->face == face && card->suit == suit){
-      return card;
-    }
-    card = card->next;
-  }
-  return NULL;
-}
+// // create a new deck of cards
+// void createDeck(struct card_node *deck){
+//   struct card_node *current = deck;
+//   for (int suit = 0; suit < NUM_SUITS; suit++){
+//       for (int face = 0; face < NUM_FACES; face++){
+//           struct card_node *newCard = createCard(face, suit, NULL);
+//           if (current == NULL){
+//               deck = newCard;
+//               current = deck;
+//           }
+//           else {
+//               current->next = newCard;
+//               current = newCard;
+//           }
+//       }
+//   }
+// }
+
+// struct card_node* insert(struct card_node *deck, int face, int suit){
+//   struct card_node* newCard = createCard(face, suit, deck);
+//   if (newCard == NULL){
+//     return deck;
+//   }
+//   return newCard;
+// }
+
+// struct card_node * random_card(struct card_node *card){
+//   if (card == NULL) return NULL;
+//   int count = 0;
+//   struct card_node * temp = card;
+//   while (temp != NULL){
+//     count++;
+//     temp = temp->next;
+//   }
+//   int end = rand() % count;
+//   for (int i = 0; i < end; i++){
+//     card = card->next;
+//   }
+//   return card;
+// }
+
+// struct card_node * remove_card(struct card_node *deck, int face, int suit){
+//   struct card_node *current = deck;
+//   struct card_node *prev = NULL;
+//   while (current){
+//     if (current->face == face && current->suit == suit){
+//       if (prev){
+//         prev->next = current->next;
+//       }
+//       else {
+//         deck = current->next;
+//       }
+//       free(current);
+//       return deck;
+//     }
+//     prev = current;
+//     current = current->next;
+//   }
+//   return deck;
+// }
