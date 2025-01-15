74e1186c2f921fe4a636c61c198597c9d0719461
David Chen <dchen07@protonmail.com>
2025-01-15 15:31:24 -0500

    Fix extraneous dealer draws

--- server.c
+++ server.c
@@ -230,7 +230,7 @@ void play(int to_client, int from_client, char * name) {
 		return;
 	}
 
-	if (!(player_blackjack || dealer_blackjack)) {
+	if (!(player_blackjack || dealer_blackjack) && player_total < 21) {
 		while (dealer_total < 17 && dealer_total <= player_total && _deck->size > 0) {
 			current = dealRandomCard(_deck);
 			addCardToHand(&dealer_hand, current);
