4eb98ecafb299e1b8f9e6228dbde07e436faa942
David Chen <dchen65535@gmail.com>
2025-01-15 16:51:38 -0500

    Add client flavor text

--- client.c
+++ client.c
@@ -25,6 +25,23 @@ void displayIntro() {
 	printf("\n\n****\nBlackjack\n\nTry to get a score of 21, but no higher!\nFace cards are worth 10 points, and the Ace is worth 1 or 11 depending on what benefits you.\nHit to draw a new card, Stand to let the dealer draw.\n****\n\n");
 }
 
+const char * eggs[] = {
+	"vanilla",
+	"chocolatey",
+	"minty",
+	"cookiesandcreamy",
+	"strawberry",
+	NULL
+};
+// https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit
+const char * sgge[] = {
+	"\033[m",
+	"\033[m\033[38;5;130m",
+	"\033[m\033[38;5;86m",
+	"\033[m\033[38;5;0m\033[48;5;15m\033[1m",
+	"\033[m\033[38;5;213m",
+	NULL
+};
 enum Action readMainMenu() {
 	char *ptr = NULL;
 	size_t size;
@@ -34,6 +51,17 @@ enum Action readMainMenu() {
 		fflush(stdout);
 		ptr = NULL;
 		safe_getline(&ptr, &size, stdin);
+		const char ** egg = eggs;
+		const char ** gge = sgge;
+		while (*egg && *gge) {
+			if (!strncasecmp(ptr, *egg, strlen(*egg))) {
+				printf("%s", *gge);
+				break;
+			}
+			egg++;
+			gge++;
+		}
+		if (*egg) continue;
 		switch (ptr[0]) {
 			case '1':
 				ret = PLAY;
